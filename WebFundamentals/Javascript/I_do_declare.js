e4e92da9ad4cd58d279eca2900c0152a7a19dd45
sam hur <gjqorgus900327@gmail.com>
2017-01-30 13:46:57 -0800

    Update Java script assignment

---
+++ WebFundamentals/Javascript/I_do_declare.js
@@ -0,0 +1,18 @@
+var num1=1;
+var num2=2;
+var num3=3;
+var num4=4;
+
+var string1="sam";
+var string2="Backhyun";
+var string3="Hur";
+
+var amIsleepy= true;
+var amihappy= false;
+
+var undefinedvar;
+
+
+console.log("The first name:",string1);
+console.log("korean name:",string2);
+console.log("The last name:",string3);
---
+++ WebFundamentals/Javascript/if_you_dont_mind.js
@@ -0,0 +1,68 @@
+
+
+var HOUR = 8;
+var MINUTE = 50;
+var PERIOD = "am";
+if(MINUTE>30 && PERIOD =="PM"){
+    console.log("It's almost",HOUR+1,"in the evening");
+}
+else if(MINUTE>30 && PERIOD =="am"){
+    console.log("It's almost",HOUR+1,"in the morning");
+}
+else if(MINUTE<30 && PERIOD =="PM"){
+    console.log("just after",HOUR,"in the evening");
+}
+else if(MINUTE<30 && PERIOD =="am"){
+    console.log("just after",HOUR,"in the morning");
+}
+
+
+var HOUR = 7;
+var MINUTE = 50;
+var PERIOD = "PM";
+
+if(MINUTE>30 && PERIOD =="PM"){
+    console.log("It's almost",HOUR+1,"in the evening");
+}
+else if(MINUTE>30 && PERIOD =="am"){
+    console.log("It's almost",HOUR+1,"in the morning");
+}
+else if(MINUTE<30 && PERIOD =="PM"){
+    console.log("just after",HOUR,"in the evening");
+}
+else if(MINUTE<30 && PERIOD =="am"){
+    console.log("just after",HOUR,"in the morning");
+}
+
+var HOUR =4;
+var MINUTE =10;
+var PERIOD="am";
+
+if(MINUTE>30 && PERIOD =="PM"){
+    console.log("It's almost",HOUR+1,"in the evening");
+}
+else if(MINUTE>30 && PERIOD =="am"){
+    console.log("It's almost",HOUR+1,"in the morning");
+}
+else if(MINUTE<30 && PERIOD =="PM"){
+    console.log("just after",HOUR,"in the evening");
+}
+else if(MINUTE<30 && PERIOD =="am"){
+    console.log("just after",HOUR,"in the morning");
+}
+
+var HOUR=2;
+var MINUTE= 20;
+var PERIOD= "PM";
+if(MINUTE>30 && PERIOD =="PM"){
+    console.log("It's almost",HOUR+1,"in the evening");
+}
+else if(MINUTE>30 && PERIOD =="am"){
+    console.log("It's almost",HOUR+1,"in the morning");
+}
+else if(MINUTE<30 && PERIOD =="PM"){
+    console.log("just after",HOUR,"in the evening");
+}
+else if(MINUTE<30 && PERIOD =="am"){
+    console.log("just after",HOUR,"in the morning");
+}
